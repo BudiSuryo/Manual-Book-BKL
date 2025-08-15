c1ae41ba920ed250b1da0ba002c68d741f931344
BudiSuryo <wmbudisp@gmail.com>
2025-08-15 16:40:03 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
 // Proteksi halaman dari akses langsung
-if (sessionStorage.getItem("isLoggedIn") !== "true") {
-    window.location.href = "index.html"; // kembali ke halaman login
+
 }// script.js placeholder
